582468187d145c34ae9e4fbdbc236bf5cdb3d12f
Vu Nguyen <vu.nguyen101@gmail.com>
2021-10-15 14:50:02 +0300

    config middleware into app

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import itemRouter from './items/items.router'
+import { errorHandler } from './middleware/error.middleware'
+import { notFoundHandler } from './middleware/not-found.middleware'
 
 dotenv.config()
 
@@ -27,6 +29,9 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json())
 app.use('/api/menu/items', itemRouter)
+
+app.use(errorHandler)
+app.use(notFoundHandler)
 
 /**
  * Server Activation
